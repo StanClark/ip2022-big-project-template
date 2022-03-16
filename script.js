eb20ed1d25a0dd5697b7fae63a5cdc84550abb76
StanClark <stan.clark03@icloud.com>
2022-03-16 23:58:43 +0000

    week 2

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const completeList = document.getElementById('complete-list');
 const onHoldList = document.getElementById('on-hold-list');
 
 // Items
-
+let updatedOnLoad = false;
 
 // Initialize Arrays
 let backlogListArray = [];
@@ -19,7 +19,8 @@ let completeListArray = [];
 let onHoldListArray = [];
 
 // Drag Functionality
-
+let draggedItem
+let currentColumn
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -52,14 +53,46 @@ function createItemEl(columnEl, column, item, index) {
   console.log('index:', index);
   // List Item
   const listEl = document.createElement('li');
+
+  listEl.textContent = item;
+  columnEl.appendChild(listEl)
+
   listEl.classList.add('drag-item');
+  listEl.draggable = true;
+  listEl.setAttribute('ondragstart','drag(event)');
+  
 
 }
 
 // Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
 function updateDOM() {
   // Check localStorage once
+  if(!updatedOnLoad){
+    getSavedColumns();
+  }
 
+  backlogList.textContent ="";
+  backlogListArray.forEach((backlogItem, index) => {
+    createItemEl(backlogList, 0, backlogItem, index);
+  })
+
+  progressList.textContent ="";
+  progressListArray.forEach((progressItem, index) => {
+    createItemEl(progressList, 1, progressItem, index);
+  })
+
+  completeList.textContent ="";
+  completeListArray.forEach((completeItem, index) => {
+    createItemEl(completeList, 2, completeItem, index);
+  })
+
+  onHoldList.textContent ="";
+  onHoldListArray.forEach((onHoldItem, index) => {
+    createItemEl(onHoldList, 3, onHoldItem, index);
+  })
+  
+  
+  
   // Backlog Column
 
   // Progress Column
@@ -73,3 +106,6 @@ function updateDOM() {
 
 }
 
+function drag(e){
+  draggedItem = e.target;
+}
